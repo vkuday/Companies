1d87bc0a6023baeb72359cbd51f01850ecdb3a9f
vkuday <vkuday@hotmail.com>
2022-07-14 23:08:33 -0700

    Initial Commit

---
+++ Companies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  Product: 
+//
+//  Created by Volkan Kuday on 7/14/22 11:08 PM.
+//  Copyright © 2022. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
